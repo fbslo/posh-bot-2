34c1ada2c80b4459a0edab555d27c4b0902e42b1
fbslo <admin@fbslo.net>
2020-12-30 16:54:42 +0100

    uncomment

--- index.js
+++ index.js
@@ -25,43 +25,43 @@ async function main(){
   const dailyPost = require("./scripts/dailyPost.js")
 
   console.log("Posh bot is waking up...")
-  // streamHiveBlockchain.start((registrationData) => {
-  //   if (registrationData.isSuccess == true) {
-  //     storeUserToDatabase.isUserAlreadyStored(registrationData.twitterUsername, registrationData.hiveUsername)
-  //       .then((result) => {
-  //         if (result != 'user_not_stored') return;
-  //         else {
-  //           //verify registration tweet and store user
-  //           verifyRegistrationTweet.verify(registrationData.twitterTweetId, registrationData.hiveUsername)
-  //             .then((result) => {
-  //               if (result != false){
-  //                 storeUserToDB(storeUserToDatabase, hiveReply, registrationData)
-  //               } else {
-  //                 //hiveReply.reply(`@${registrationData.hiveUsername}, your Tweet doesen't seems to be in the correct format!`, registrationData)
-  //               }
-  //             })
-  //             .catch((err) => {
-  //               console.log(err)
-  //               //hiveReply.reply(`@${registrationData.hiveUsername}, there was an error while processing your request. Please try again later.`, registrationData)
-  //             })
-  //         }
-  //       })
-  //   }
-  //   else hiveReply.reply(`@${registrationData.hiveUsername}, there was an error while processing your request. Please try again later.`, registrationData)
-  // })
-  //
-  // streamTwitter.start((tweetData) => {
-  //   if (!tweetData.retweeted_status){ //is not a retweet
-  //     doesTweetIncludeHiveLink.check(tweetData)
-  //       .then((result) => {
-  //         if (result == false) return; //console.log(`Tweet by ${tweetData.user.screen_name} does not include Hive link.`)
-  //         else storeTweetToDatabase.store(tweetData, result).then((result) => {
-  //           if (result == 'not_found') return; //console.log(`User @${tweetData.user.screen_name} is not registered!`)
-  //           else console.log(`Tweet @${tweetData.user.screen_name}/${tweetData.id_str} stored.`)
-  //         })
-  //       })
-  //   }
-  // })
+  streamHiveBlockchain.start((registrationData) => {
+    if (registrationData.isSuccess == true) {
+      storeUserToDatabase.isUserAlreadyStored(registrationData.twitterUsername, registrationData.hiveUsername)
+        .then((result) => {
+          if (result != 'user_not_stored') return;
+          else {
+            //verify registration tweet and store user
+            verifyRegistrationTweet.verify(registrationData.twitterTweetId, registrationData.hiveUsername)
+              .then((result) => {
+                if (result != false){
+                  storeUserToDB(storeUserToDatabase, hiveReply, registrationData)
+                } else {
+                  //hiveReply.reply(`@${registrationData.hiveUsername}, your Tweet doesen't seems to be in the correct format!`, registrationData)
+                }
+              })
+              .catch((err) => {
+                console.log(err)
+                //hiveReply.reply(`@${registrationData.hiveUsername}, there was an error while processing your request. Please try again later.`, registrationData)
+              })
+          }
+        })
+    }
+    else hiveReply.reply(`@${registrationData.hiveUsername}, there was an error while processing your request. Please try again later.`, registrationData)
+  })
+
+  streamTwitter.start((tweetData) => {
+    if (!tweetData.retweeted_status){ //is not a retweet
+      doesTweetIncludeHiveLink.check(tweetData)
+        .then((result) => {
+          if (result == false) return; //console.log(`Tweet by ${tweetData.user.screen_name} does not include Hive link.`)
+          else storeTweetToDatabase.store(tweetData, result).then((result) => {
+            if (result == 'not_found') return; //console.log(`User @${tweetData.user.screen_name} is not registered!`)
+            else console.log(`Tweet @${tweetData.user.screen_name}/${tweetData.id_str} stored.`)
+          })
+        })
+    }
+  })
 
   schedule.scheduleJob('30 * * * *', () => {
     calculateEngagementScore.calculate()
